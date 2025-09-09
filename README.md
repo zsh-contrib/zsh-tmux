442ff2437fae62402f619ca3755e9ec70d3f4435
Svetlin Ralchev <iamralch@users.noreply.github.com>
2025-09-09 14:34:31 +0400

    feat: add initial zsh-tmux plugin and README
    Added the initial implementation of the zsh-tmux plugin, including
    functions to update the tmux window title based on shell activity. Also
    added a README with installation instructions.
    
    Signed-off-by: Svetlin Ralchev <iamralch@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,11 @@
+# zsh-tmux
+
+A tmux plugin for zsh
+
+## Installation
+
+Using zinit, add the following line to your `.zshrc`:
+
+```zsh
+zinit load zsh-contrib/zsh-tmux
+```
---
+++ zsh-tmux.plugin.zsh
@@ -0,0 +1,58 @@
+#!/usr/bin/env zsh
+#
+function update_title() {
+  emulate -L zsh -o extendedglob
+  setopt localoptions no_shwordsplit
+
+  # parameters
+  local title
+
+  # prepare the title
+  title=${(V)1//\%/\%\%}
+  title=$(print -n -- "%20>...>$title")
+  title=${title//$'\n'/}
+
+  printf '\033k%s\' "${(%)title}"
+}
+
+# called just before the prompt is printed
+function _zsh_title__precmd() {
+  emulate -L zsh
+  update_title "zsh"
+}
+
+# called just before a command is executed
+function _zsh_title__preexec() {
+  emulate -L zsh
+  setopt localoptions no_shwordsplit
+
+  # Work on a copy; don't assign to $1.
+  local line=$1
+
+  # Escape backslashes so ${(z)...} re-parses correctly.
+  line=${line//\\/\\\\\\\\}
+
+  # Re-parse the command line into words
+  local -a words
+  words=(${(z)line})
+
+  # Resolve job references (fg or %N) to their real command text, if available.
+  local job_spec
+  case $words[1] in
+    fg) job_spec=${(Q)${words[2]:-%+}} ;;
+    %*) job_spec=${(Q)words[1]} ;;
+  esac
+
+  if [[ -n $job_spec && -n ${jobtexts[$job_spec]} ]]; then
+    words=(${(z)jobtexts[$job_spec]})
+  fi
+
+  # Join words for display
+  local cmd_name=${(j: :)words}
+  update_title "$cmd_name"
+}
+
+autoload -Uz add-zsh-hook
+
+add-zsh-hook precmd _zsh_title__precmd
+add-zsh-hook preexec _zsh_title__preexec
