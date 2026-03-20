b8501840711e3534e977f4f2e61c715f85ba2a53
Svetlin Ralchev <iamralch@users.noreply.github.com>
2026-03-20 10:30:24 +0400

    Add Nix flake with dev shell

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "zsh-tmux development shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "zsh-tmux";
+          packages = with pkgs; [
+            zsh
+            bats
+            shellcheck
+            tmux
+          ];
+        };
+      }
+    );
+}
